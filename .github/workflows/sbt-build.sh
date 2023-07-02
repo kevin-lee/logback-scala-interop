435f1c25df0f7b2006ab8dd642a87cbc5348019d
Kevin Lee <kevin.code@kevinlee.io>
2023-07-02 21:35:49 +1000

    Add GitHub Actions for build and release - build.yml with build script, sbt-build.sh - release.yml - pr-labeler.yml - dependabot.yml

---
+++ .github/workflows/sbt-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+set -x
+
+if [ -z "$1" ]
+  then
+    echo "Scala version is missing. Please enter the Scala version."
+    echo "sbt-build.sh 2.13.11"
+    exit 1
+else
+  SCALA_VERSION=$1
+  echo "============================================"
+  echo "Build projects"
+  echo "--------------------------------------------"
+  echo ""
+  if [[ "$CI_BRANCH" == "main" || "$CI_BRANCH" == "release" ]]
+  then
+    sbt -d -J-Xmx2048m ++${SCALA_VERSION}! clean compile packagedArtifacts
+  else
+    sbt -d -J-Xmx2048m ++${SCALA_VERSION}! clean compile package
+  fi
+
+  echo "============================================"
+  echo "Building projects: Done"
+  echo "============================================"
+fi
